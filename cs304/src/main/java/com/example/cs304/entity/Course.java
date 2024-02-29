b0eb44220b6051fa16c799385811963fd14aa803
OasisP <2056429129@qq.com>
2024-02-29 23:27:26 +0800

    修改了Course类的属性

--- cs304/src/main/java/com/example/cs304/entity/Course.java
+++ cs304/src/main/java/com/example/cs304/entity/Course.java
@@ -25,13 +25,44 @@ public class Course implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
-    private String teachingClas;
+    private String teachingClass;
 
     private String traingType;
 
     private String courseCode;
 
-    private String courseName;
+    private String courseNature;//课程性质
+
+    private String courseCategory;
+
+    private String language;
+
+    private Double credits;
+
+    private Double hours;
+
+    private String department; //开课院系
+
+    private String semester; // 开课学期
+
+    private String gradeType;
+
+    private Integer capacity;
+
+    private String chineseName;
+
+    private String englishName;
+
+    // 上课信息
+    private String requirement; //选课要求
+
+    private String teacher; //老师名字
+
+    private String teachingBuilding; //位置
+
+    private Integer xq; //星期几
+
+    private Integer jc; //节次
 
 
 }
