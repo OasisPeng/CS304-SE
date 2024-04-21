e287a8bce70fd17299b369dfcdfd0006526992cb
OasisP <2056439129@qq.com>
2024-04-21 17:47:26 +0800

    登录接口

--- cs304/src/main/java/com/example/cs304/controller/LoginController.java
+++ cs304/src/main/java/com/example/cs304/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.example.cs304.controller;
 
+import com.example.cs304.common.JwtUtil;
 import com.example.cs304.common.Result;
 import com.example.cs304.common.Util;
 import com.example.cs304.entity.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,16 @@ import java.net.URISyntaxException;
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+    @Value("${jwt.secret-key}")
+    String secretKey;
     @PostMapping("/login")
-    public Result login(@RequestBody Student student) throws IOException, URISyntaxException {
-        String[] cookies =  Util.casLogin(student.getUsername(), student.getPassword());
-        if (cookies[0].equals("")) {
+    public Result login(@RequestBody Student student) {
+        if (!Util.loginVerify(student.getUsername(), student.getPassword())) {
             return Result.fail();
         } else {
-            return Result.suc(cookies);
+            String token = JwtUtil.generateToken(secretKey, student.getUsername());
+            student.setToken(token);
+            return Result.suc(student);
         }
     }
 }
