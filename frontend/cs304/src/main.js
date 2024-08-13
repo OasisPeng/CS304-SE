c41be97c90a791bea56b64edca9ecd55c8bfc8e1
OasisP <2056439129@qq.com>
2024-08-13 21:36:44 +0800

    ip修改

--- frontend/cs304/src/main.js
+++ frontend/cs304/src/main.js
@@ -5,8 +5,8 @@ import vuetify from './plugins/vuetify'
 import axios from "axios";
 import './permission.js'
 Vue.prototype.$axios=axios;
-Vue.prototype.$httpUrl='http://localhost:8090'
-// Vue.prototype.$httpUrl='http://8.138.27.126:8090'
+// Vue.prototype.$httpUrl='http://localhost:8090'
+Vue.prototype.$httpUrl='http://8.138.27.126:8090'
 Vue.config.productionTip = false
 localStorage.clear()
 new Vue({
